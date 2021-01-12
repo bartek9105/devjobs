2091b7d5bbb4fe284b83064758c8a393b9666d50
bartek9105 <bartek9105@gmail.com>
2021-01-12 18:21:16 +0100

    Make logo a link, add loading spinner for offer details page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import './Header.scss'
 import Logo from '../Logo'
 import Switch from '../Switch/Switch'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header className="main-header">
       <div className="header-container">
-        <Logo />
+        <Link to="/">
+          <Logo />
+        </Link>
         <Switch />
       </div>
     </header>
--- src/pages/JobDetails.js
+++ src/pages/JobDetails.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Header from '../components/Header/Header'
 import Button from '../components/Button/Button'
+import Spinner from '../components/Spinner/Spinner'
 import './JobDetails.scss'
 import calcDateFromNow from '../helpers/calcDateFromNow'
 
@@ -9,11 +10,13 @@ const JobDetails = ({ match }) => {
   const API_URL = `${process.env.REACT_APP_CORS_ANYWHERE}/https://jobs.github.com/positions/${match.params.id}.json`
 
   const [job, setJob] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
 
   const fetchJob = async () => {
     try {
       const job = await axios.get(API_URL)
       setJob(job.data)
+      setIsLoading(false)
     } catch (error) {
       console.log(error)
     }
@@ -26,51 +29,56 @@ const JobDetails = ({ match }) => {
   return (
     <section>
       <Header />
-      <header className='job-details-header'>
-        <div className='job-details-header__logo'>
-          <img src={job.company_logo} alt={job.company} />
-        </div>
-        <div className='job-details-header__company'>
-          <div>
-            <h1 className='job-details-header__company__name'>{job.company}</h1>
-            <span className="job-details-header__company__url">{job.company_url}</span>
-          </div>
-          <Button btnText="Company Site" />
-        </div>
-      </header>
-      <section className='job-details-container'>
-        <article>
-          <header className="job-details-container__header">
-            <div className="job-details-container__header__info">
-              <span>{calcDateFromNow(job.created_at)}</span>
-              <span className="job-details-container__header__info__bullet">&bull;</span>
-              <span>{job.type}</span>
+      { isLoading ? <Spinner /> : (
+        <>
+          <header className='job-details-header'>
+            <div className='job-details-header__logo'>
+              <img src={job.company_logo} alt={job.company} />
+            </div>
+            <div className='job-details-header__company'>
               <div>
-                <h1 className="job-details-container__header__title">{job.title}</h1>
-                <span className="job-details-container__header__location">{job.location}</span>
+                <h1 className='job-details-header__company__name'>{job.company}</h1>
+                <span className="job-details-header__company__url">{job.company_url}</span>
+              </div>
+              <Button btnText="Company Site" />
+            </div>
+          </header>
+          <section className='job-details-container'>
+            <article>
+              <header className="job-details-container__header">
+                <div className="job-details-container__header__info">
+                  <span>{calcDateFromNow(job.created_at)}</span>
+                  <span className="job-details-container__header__info__bullet">&bull;</span>
+                  <span>{job.type}</span>
+                  <div>
+                    <h1 className="job-details-container__header__title">{job.title}</h1>
+                    <span className="job-details-container__header__location">{job.location}</span>
+                  </div>
+                </div>
+                <Button btnText="Apply Now" />
+              </header>
+              <div className="job-details-container__description" dangerouslySetInnerHTML={{ __html: job.description }}>
+              </div>
+            </article>
+          </section>
+          <section className="job-details-apply">
+            <h2 className="job-details-apply__title">How to apply</h2>
+            <article dangerouslySetInnerHTML={{ __html: job.how_to_apply }}></article>
+          </section>
+          <footer className="job-details-footer">
+            <div className="job-details-footer-container">
+              <div className="job-details-footer__info">
+                <span className="job-details-footer__info__title">{job.title}</span>
+                <span className="job-details-footer__info__company">{job.company}</span>
+              </div>
+              <div>
+                <Button btnText="Apply Now" />
               </div>
             </div>
-            <Button btnText="Apply Now" />
-          </header>
-          <div className="job-details-container__description" dangerouslySetInnerHTML={{ __html: job.description }}>
-          </div>
-        </article>
-      </section>
-      <section className="job-details-apply">
-        <h2 className="job-details-apply__title">How to apply</h2>
-        <article dangerouslySetInnerHTML={{ __html: job.how_to_apply }}></article>
-      </section>
-      <footer className="job-details-footer">
-        <div className="job-details-footer-container">
-          <div className="job-details-footer__info">
-            <span className="job-details-footer__info__title">{job.title}</span>
-            <span className="job-details-footer__info__company">{job.company}</span>
-          </div>
-          <div>
-            <Button btnText="Apply Now" />
-          </div>
-        </div>
-      </footer>
+          </footer>
+        </>
+      )
+      }
     </section>
   )
 }
